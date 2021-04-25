0a49118c77082e8bfb4581740912f84dccc2cda8
hr-john <80419362+hr-john@users.noreply.github.com>
2021-04-26 02:16:56 +0300

    Update kalender.js

--- kalender.js
+++ kalender.js
@@ -76,7 +76,7 @@ var cal = {
       else {
         cCell.innerHTML = "<div class='dd'>"+squares[i]+"</div>";
         cCell.addEventListener("click", function vaartus(){
-          kp = cal.sYear + "-" + mNr[cal.sMth] + "-" + this.getElementsByClassName("dd")[0].innerHTML;
+          kp = cal.sYear + "-" + cal.mNr[cal.sMth] + "-" + this.getElementsByClassName("dd")[0].innerHTML;
           alert ( "KP" + kp ) ;
         });
       }
