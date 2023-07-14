73e400346bf0b14183612324a03cf385e2f7331a
hr-john <80419362+hr-john@users.noreply.github.com>
2023-07-14 09:47:23 +0300

    Update index.js
    Võrk 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
 function arvuta() {
     const hind = [ 
-    [1.13, 0.447,  5.12, 6.16, 3.58, 4.23, 2.46, 2.28, 2.74, 1.58],
-    [1.13, 0.1,  5.12, 6.16, 3.58, 4.23, 2.46, 2.28, 2.74, 1.58,],
-    [1.04, 0.447, 5.28, 6.35, 3.69, 5.13, 2.98, 2.98, 3.58, 2.06],
-    [0.89, 0.447, 5.28, 6.35, 3.69, 5.13, 2.98, 2.98, 3.58, 2.06],
-    [1.13, 0.1,  5.12, 6.16, 3.58, 4.23, 2.46, 2.28, 2.74, 1.58],
-    [1.13, 0.1,  5.53, 6.65, 3.86, 4.35, 2.52, 999, 2.83, 1.62],
-    [1.13, 0.1,  6.26, 7.53, 4.38, 4.92, 2.85, 999, 3.20, 1.83],
-    [1.13, 0.1,  6.38, 7.68, 4.47, 5.02, 2.90, 999, 3.26, 1.86],
-    [1.24, 0.1,  7.21, 8.68, 5.05, 5.67, 3.28, 999, 3.69, 2.10],
-    [1.13, 0.1,  7.21, 8.68, 5.05, 5.67, 3.28, 999, 3.69, 2.10]
+    [1.13, 0.447,5.12, 6.16, 3.58, 4.23, 2.46, 2.28, 2.74, 1.58, 999, 999, 999, 999],
+    [1.13, 0.1,  5.12, 6.16, 3.58, 4.23, 2.46, 2.28, 2.74, 1.58, 999, 999, 999, 999],
+    [1.04, 0.447,5.28, 6.35, 3.69, 5.13, 2.98, 2.98, 3.58, 2.06, 999, 999, 999, 999 ],
+    [0.89, 0.447,5.28, 6.35, 3.69, 5.13, 2.98, 2.98, 3.58, 2.06, 999, 999, 999, 999],
+    [1.13, 0.1,  5.12, 6.16, 3.58, 4.23, 2.46, 2.28, 2.74, 1.58, 999, 999, 999, 999],
+    [1.13, 0.1,  5.53, 6.65, 3.86, 4.35, 2.52, 999,  2.83, 1.62, 3.95, 6.07, 2.26, 3.52],
+    [1.13, 0.1,  6.26, 7.53, 4.38, 4.92, 2.85, 999,  3.20, 1.83, 4.47, 6.88, 2.57, 3.99],
+    [1.13, 0.1,  6.38, 7.68, 4.47, 5.02, 2.90, 999,  3.26, 1.86, 4.55, 7.02, 2.61, 4.07],
+    [1.24, 0.1,  7.21, 8.68, 5.05, 5.67, 3.28, 999,  3.69, 2.10, 5.14, 7.94, 2.95, 4.60],
+    [1.13, 0.1,  7.21, 8.68, 5.05, 5.67, 3.28, 999,  3.69, 2.10, 5.14, 7.94, 2.95, 4.60]
     ];
     const kuutasu = [ 
     [
@@ -74,29 +74,37 @@ function arvuta() {
     ]
     ];
     const aasta = document.getElementById("aasta").value;
+    let p = document.getElementById("p").value;
+    let o = document.getElementById("o").value;
+    let pt = document.getElementById("pt").value;
+    let ot = document.getElementById("ot").value;
+    let kaitse = document.getElementById("kaitse").value;
     
     document.getElementById('tt').value = Math.round(Number(document.getElementById("kokkukW").value) * Number(hind[aasta][0])*100)/100;
     document.getElementById('ak').value = Math.round(Number(document.getElementById("kokkukW").value) * Number(hind[aasta][1])*100)/100;
     document.getElementById('v1').value = Math.round(Number(document.getElementById("kokkukW").value) * Number(hind[aasta][2])*100)/100;
-    document.getElementById('v2').value = Math.round( ( Number(document.getElementById("p").value) * Number(hind[aasta][3]) + Number(document.getElementById("o").value) * Number(hind[aasta][4]) )*100)/100;
+    document.getElementById('v2').value = Math.round( ( Number(p+pt) * Number(hind[aasta][3]) + Number(o+ot) * Number(hind[aasta][4]) )*100)/100;
+    document.getElementById('vk2').value = Math.round( ( (Number(p)+Number(pt)) * Number(hind[aasta][5]) + Number(o+ot) * Number(hind[aasta][6]) )*100)/100 + kuutasu[aasta][1][document.getElementById("kaitse").value];
     document.getElementById('v3').value = Math.round( ( Number(document.getElementById("kokkukW").value) * Number(hind[aasta][7]) )*100)/100 + kuutasu[aasta][2][document.getElementById("kaitse").value];
     document.getElementById('v4').value = Math.round( ( Number(document.getElementById("p").value) * Number(hind[aasta][8]) + Number(document.getElementById("o").value) * Number(hind[aasta][9]) )*100)/100 + kuutasu[aasta][2][document.getElementById("kaitse").value];
-    document.getElementById('vk2').value = Math.round( ( Number(document.getElementById("p").value) * Number(hind[aasta][5]) + Number(document.getElementById("o").value) * Number(hind[aasta][6]) )*100)/100 + kuutasu[aasta][1][document.getElementById("kaitse").value];
+    document.getElementById('v5').value = Math.round( ( Number(p) * Number(hind[aasta][10]) + Number(pt) * Number(hind[aasta][11]) + Number(o) * Number(hind[aasta][12]) + Number(ot) * Number(hind[aasta][13]) )*100)/100 + kuutasu[aasta][3][kaitse];
     const tt_ak= document.getElementById('tt').value + document.getElementById('ak').value;
     
     document.getElementById('v1').innerHTML = document.getElementById('v1').value;
     document.getElementById('v2').innerHTML = document.getElementById('v2').value;
+    document.getElementById('vk2').innerHTML = document.getElementById('vk2').value;
     document.getElementById('v3').innerHTML = document.getElementById('v3').value;
     document.getElementById('v4').innerHTML = document.getElementById('v4').value;
-    document.getElementById('vk2').innerHTML = document.getElementById('vk2').value;
+    document.getElementById('v5').innerHTML = document.getElementById('v5').value;
     document.getElementById('tt').innerHTML = document.getElementById('tt').value;
     document.getElementById('ak').innerHTML = document.getElementById('ak').value;
 
     document.getElementById('v1k').innerHTML = Math.round(1.2 * (Number(document.getElementById("v1").value) + tt_ak ) );
     document.getElementById('v2k').innerHTML = Math.round(1.2 * (Number(document.getElementById("v2").value) + tt_ak ) );
+    document.getElementById('vk2k').innerHTML = Math.round(1.2 * (Number(document.getElementById("vk2").value) + tt_ak ) );
     document.getElementById('v3k').innerHTML = Math.round(1.2 * (Number(document.getElementById("v3").value) + tt_ak ) );
     document.getElementById('v4k').innerHTML = Math.round(1.2 * (Number(document.getElementById("v4").value) + tt_ak ) );
-    document.getElementById('vk2k').innerHTML = Math.round(1.2 * (Number(document.getElementById("vk2").value) + tt_ak ) ); 
+    document.getElementById('v5k').innerHTML = Math.round(1.2 * (Number(document.getElementById("v5").value) + tt_ak ) );
 }
 
 function summa() {
